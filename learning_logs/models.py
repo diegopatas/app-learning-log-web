92f30be7ea01474ce1ec13025a292102dcb00030
Diego Diniz <oitopatas.email@gmail.com>
2021-11-22 18:03:22 -0300

    Add a class to manage entries associated to topics.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -11,3 +11,20 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+        def __str__(self):
+            """Return a string representation of the model."""
+            return  f"{self.text[:50]}..."
+
+    # _explanation
+    # We create an instance of ForeingKey (a way connect a topic to an entry); it has a cascading delete property
+    # We create an instance of textfield
+    # date_added allows us to present entries in order of creation.
+    # Meta is a nest class that manages extra information, in this case, a plural.
+    # Meta returns content with max 50 char.
