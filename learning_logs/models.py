265c62c6536db1b728db4f762d19d7e4f6710453
Diego Diniz <oitopatas.email@gmail.com>
2021-11-22 17:47:27 -0300

    Initial commit.

---
+++ learning_logs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Topic(models.Model):
+    """A topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
