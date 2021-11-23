24335ef4f27a88eb5f4cb5d1389b2951928a5db9
Diego Diniz <oitopatas.email@gmail.com>
2021-11-23 17:56:27 -0300

    Add an entry form, a new view function (new_entry method), and creates a URLs pattern.

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -3,8 +3,16 @@ from django import forms
 from .models import Topic
 
 class TopicForm(forms.ModelForm):
-    """A form to accept user input data."""
+    """A form to accept a new topic adding."""
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    """A form to accept a new entry adding."""
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
