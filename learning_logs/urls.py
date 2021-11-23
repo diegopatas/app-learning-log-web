bafbddb5e4b414d3a536f76d02693f4aafa82b96
Diego Diniz <oitopatas.email@gmail.com>
2021-11-22 23:18:55 -0300

    Just cc.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,8 +8,3 @@ urlpatterns = [
     # Home page
     path('', views.index, name='index'),
 ]
-
-# _explanation
-# The dot means that we are importing views from the same directory as this file
-# The variable app_name helps django to distinguish this file from others with the same name
-# The urlpatterns is a list with pages that can be requested from the app
